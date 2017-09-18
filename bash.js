5e9b772df1880166e006b8171abcc1a59da08cdd
cyng24 <cyng24@gmail.com>
2017-09-18 15:21:27 -0400

    latest update

--- bash.js
+++ bash.js
@@ -4,22 +4,28 @@ process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types in a line
 
 process.stdin.on('data', function (data, done) {
-  var done = function(output) {
-    process.stdout.write(output);
-    process.stdout.write('\nprompt > ');
+  var done = function(output, boolean = false) {
+    if (!boolean){
+        process.stdout.write(output);
+        process.stdout.write('\nprompt > ');
+    }
+    else {
+        return output
+    }
   }  
-var pipeArray = data.toString().trim().split(/\s*\|\s*/g); // remove the newline, any amount of whitespace, pipe, any amount of whitespace
-var cmd = pipeArray[0].split(' ')
-if(pipeArray.length == 1){
-    commands[cmd[0]](cmd.slice(1), done);
+var cmdList = data.toString().trim().split(/\s*\|\s*/g); // remove the newline, any amount of whitespace, pipe, any amount of whitespace
+var cmd= cmdList[0].split(' ');
+if(cmdList.length == 1){
+    commands[cmd[0]](stdin = false, cmd.slice(1), done);
 }  
 else{
-    commands[cmd[1]](cmd[0], done);
+    var pipedIn = commands[cmd[0]](stdin = true, cmd.slice(1), done);
+    commands[cmdList[1]](stdin, pipedIn, done);
 }
 });
 
 // cat bash.js | head
-// pipeArray = ["cat bash.js", "head"];
-// cmd = ["cat", "bash.js"]
-// commands[bash.js](cat, done);
-// commands[head](cat bash.js, done);
+// cmdList = ["cat bash.js", "head"];
+// cmd = ["cat", "bash.js"];
+// pipedIn = all that text;
+// commands[head](all that text)
--- commands.js
+++ commands.js
@@ -2,12 +2,12 @@ var fs = require('fs');
 var request = require('request');
 
 module.exports = {
-    pwd: function(filename, done){
-        done(process.cwd());
+    pwd: function(stdin, filename, done){
+        done(process.cwd(), stdin);
     },
-    date: function(filename, done){
+    date: function(stdin, filename, done){
         var nowDate = new Date();
-        done(nowDate.toString());
+        done(nowDate.toString(), stdin);
     },
     ls: function(stdin, filename, done){
         var result = "";
@@ -16,33 +16,32 @@ module.exports = {
             files.forEach(function(file) {
                result += (file.toString() + "\n");
             })
-            done(result);
+            done(result, stdin);
         })
     },
-    echo: function(filename, done){
-        done(filename.join(' '));
+    echo: function(stdin, filename, done){
+        done(filename.join(' '), stdin);
     },
-    cat: function(filename, done){
+    cat: function(stdin, filename, done){
         fs.readFile(filename.toString(), function(err, contents){
             if (err) throw err;
-            done(contents.toString());
+            done(contents.toString(), stdin);
         })
     },
     head: function(stdin, filename, done){
-        if(stdin){done(stdin)};
         fs.readFile(filename.toString(), function(err, contents){
             if (err) throw err;
-            done(contents.toString().split('\n').slice(0,5).join('\n'));
+            done(contents.toString().split('\n').slice(0,5).join('\n'), stdin);
         })
     },
-    tail: function(filename, done){
+    tail: function(stdin, filename, done){
         fs.readFile(filename.toString(), function(err, contents){
             if (err) throw err;
             var array = contents.toString().split('\n');
-            done(array.slice(array.length-5,array.length).join('\n'));
+            done(array.slice(array.length-5,array.length).join('\n'), stdin);
         })
     },
-    curl: function(filename, done){
+    curl: function(stdin, filename, done){
         request(filename.toString(), function(error, response, body){
             var result = "";
             result += 'error: ' + error + '\n';
@@ -50,7 +49,7 @@ module.exports = {
                 result += 'statusCode: ' + response.statusCode + '\n';
             }    
             result += 'body: ' + body;
-            done(result);
+            done(result, stdin);
         })
     }
 }
